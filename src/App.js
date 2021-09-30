d6f81e5aa888df1caa2c64a89b6137033484fed4
Igor <igormgg@hotmail.com>
2021-09-30 10:45:08 -0300

    feat: libs added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./reset.css";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <main></main>
+      <main>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </main>
     </div>
   );
 }
